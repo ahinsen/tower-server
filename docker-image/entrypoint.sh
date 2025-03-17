73d5abc7715fabc6d666a68a9c8b83444ad8a9c7
Andras H <a.hinsen@gmail.com>
2025-03-17 11:29:07 +0100

    Rename iotserver1 to deviceHttpAPI

--- docker-image/entrypoint.sh
+++ docker-image/entrypoint.sh
@@ -1,14 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 # filepath: /home/iot-server/tower-server/docker-image/entrypoint.sh
-# #!/bin/bash
-
-# Modify the towerSrvCfg.json file
-# cp /home/iot-server/tower-server/towerSrvCfg_container.json /home/iot-server/tower-server/towerSrvCfg.json
-# echo "----------Modified towerSrvCfg.json----------------"
 
 # Start both node processes using forever
-forever start /home/iot-server/tower-server/docker-image/iotserver1.js
-forever start /home/iot-server/tower-server/docker-image/aioUpdate.js
+forever start /home/tower-server/deviceHttpAPI.js
+forever start /home/tower-server/aioUpdate.js
 
 # Keep the container running
 tail -f /dev/null
