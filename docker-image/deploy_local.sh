78b73c222fb31610d507251d9e77a5b9cccdcc1d
Andras H <a.hinsen@gmail.com>
2025-03-22 21:50:51 +0100

    Split compose and deploy dev & prd

--- docker-image/deploy_local.sh
+++ docker-image/deploy_local.sh
@@ -1,20 +1,29 @@
 
 #!/bin/bash
+# Check the first argument and set the USR variable
+if [ "$1" == "dev" ]; then
+    USR="iot-server"
+elif [ "$1" == "prd" ]; then
+    USR="bz"
+else
+    echo "usage: $0 dev or $0 prd"
+    exit 1
+fi
 
 IMG="towersrv"
-cd /home/iot-server/tower-server
+cd /home/${USR}/tower-server
 
 echo 'deleting files of previous builds, and removing image -----------------'
 rm -f ./docker-image/${IMG}.tar
 rm -f ./docker-image/*.js
 rm -f ./docker-image/*.json
-docker compose --file '/home/iot-server/tower-server/docker-image/docker-compose.yml' --project-name 'docker-image' down
+docker compose --file /home/${USR}/tower-server/docker-image/compose-${$1}.yml --project-name 'docker-image' down
 sudo docker rmi -f towersrv
 sudo docker image ls
 
 echo 'copying files to docker-image -----------------------------------------'
-cp /home/iot-server/tower-server/src/*.js ./docker-image/
-cp /home/iot-server/tower-server/*.json ./docker-image/
+cp /home/${USR}/tower-server/src/*.js ./docker-image/
+cp /home/${USR}/tower-server/*.json ./docker-image/
 
 # Build the docker image locally
 echo 'building image --------------------------------------------------------'
@@ -29,5 +38,5 @@ if [ "$confirm" != "y" ]; then
     echo "Deployment aborted."
     exit 1
 fi
-docker compose -f ./docker-image/docker-compose.yml up -d
+docker compose -f ./docker-image/compose-${$1}.yml up -d
 docker logs -f ${IMG}
